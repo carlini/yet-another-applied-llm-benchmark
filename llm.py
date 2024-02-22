849505c794d8c8b37c20b554a9df1f810f3716f4
Viswa <viswanatha.g15@iiits.in>
2024-02-22 07:41:24 +0000

    add gemma model

--- llm.py
+++ llm.py
@@ -26,6 +26,7 @@ from llms.anthropic_model import AnthropicModel
 from llms.mistral_model import MistralModel
 from llms.vertexai_model import VertexAIModel
 from llms.cohere_model import CohereModel
+from llms.gemma_model import GemmaModel
 
 class LLM:
     def __init__(self, name="gpt-3.5-turbo", use_cache=True, override_hparams={}):
@@ -42,6 +43,8 @@ class LLM:
             self.model = AnthropicModel(name)
         elif 'command' in name:
             self.model = CohereModel(name)
+        elif 'gemma' in name:
+            self.model = GemmaModel(name)
         else:
             raise
         self.model.hparams.update(override_hparams)
@@ -92,12 +95,14 @@ class LLM:
 
 #llm = LLM("command")
 #llm = LLM("gpt-3.5-turbo")
+# llm = LLM("gemma:2b-it")
 llm = LLM("gpt-4-1106-preview")
 #llm = LLM("claude-instant-1.2")
 #llm = LLM("mistral-tiny")
 #llm = LLM("gemini-pro", override_hparams={'temperature': 0.3}, use_cache=False)
 
 #eval_llm = LLM("gpt-4-1106-preview")
+# eval_llm = LLM("gemma:2b-it")
 eval_llm = LLM("gpt-4-0125-preview", override_hparams={'temperature': 0.1})
 #eval_llm = LLM("gpt-3.5-turbo", override_hparams={'temperature': 0.1})
 
