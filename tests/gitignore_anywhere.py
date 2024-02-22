1e92067b214914416c9b80e34fae04829ea12db6
Nicholas Carlini <nicholas@carlini.com>
2024-02-22 14:22:00 -0800

    Fix #6, gitignore_anywhere.py has incorrect question framed

--- tests/gitignore_anywhere.py
+++ tests/gitignore_anywhere.py
@@ -1,14 +1,20 @@
 from evaluator import *
 
-DESCRIPTION = "Test if the model can understand and interpret a request to gitignore any file called \".KEYFILE\" regardless of its location in a repository."
+DESCRIPTION = "Test if the model can understand and interpret a request to gitignore any file called \"foo/.KEYFILE\"  regardless of its location in a repository."
 
 TAGS = ['explain']
 
-question = """
-gitignore any file called .KEYFILE anywhere
+question1 = """
+gitignore any file called .KEYFILE directly inside a directory foo, which itself is anywhere
 """
 
-TestWhatIsStarStar = question >> LLMRun() >> SubstringEvaluator("**/.KEYFILE")
+TestWhatIsStarStar = question1 >> LLMRun() >> SubstringEvaluator("**/foo/.KEYFILE")
+
+question2 = """
+gitignore any file called .KEYFILE anywhere under a directory foo, which itself is anywhere
+"""
+
+TestWhatIsStarStarB = question2 >> LLMRun() >> SubstringEvaluator("**/foo/**/.KEYFILE")
 
 if __name__ == "__main__":
-    print(run_test(TestWhatIsStarStar))
+    print(run_test(TestWhatIsStarStarB))
